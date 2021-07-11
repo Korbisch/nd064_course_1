c15d80176b18f5c7ab2db0e7e4ed7dae2c88c7cf
Korbinian Schleifer <korbinian.schleifer@gmail.com>
2021-07-11 15:50:11 +0200

    Helm

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,9 +1,49 @@
 from flask import Flask
+import json
+import logging
+import datetime
+
 app = Flask(__name__)
+
+## stream logs to a file
+logging.basicConfig(
+    filename='app.log', 
+    level=logging.DEBUG, 
+    format='%(asctime)s %(levelname)-8s %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
 
 @app.route("/")
 def hello():
+    app.logger.info('Main request successful')
     return "Hello World!"
 
+@app.route("/status")
+def status():
+    response = app.response_class(
+        response = json.dumps({"result":"OK - healthy"}),
+        status = 200,
+        mimetype = 'application/json'
+    )
+
+    ## log line
+    app.logger.info(f'Status request successful')
+
+    return response
+
+@app.route("/metrics")
+def metrics():
+    response = app.response_class(
+        response = json.dumps({"status": "success", "code": 0, "data": {"UserCount": "140", "UserCountActive": "23"}}),
+        status = 200,
+        mimetype = 'application/json'
+    )
+
+    ## log line
+    app.logger.info(f'Metrics request successful')
+
+    return response
+
 if __name__ == "__main__":
+    
     app.run(host='0.0.0.0')
